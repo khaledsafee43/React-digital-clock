86935a4a32c1136380bc922b96a258fca5f0b964
khaledsaifee <saifeekhaled1212@gmail.com>
2025-09-09 18:46:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+// in this place imported digital clock from another component
 import DigitalClock from './clock';
 
 function App() {
   return (
     <div className="App">
+    // in this place use digital clock
       <DigitalClock/>
     </div>
   );
